01f056399ef3b6762d422a2fb56e1fb9fd95a70e
Joey Boum Bletterman <j.r.boum.bletterman@st.hanze.nl>
2016-04-02 11:37:53 +0100

    I have supplied some interface documentation.

---
+++ Customer.java
@@ -0,0 +1,11 @@
+/**
+ * Class for creating the customers that own the cars
+ * parked in the car park.
+ *
+ * @author Joey Boum Bletterman
+ * @version 0.1 (4/1/16).
+ *
+ */
+public abstract class Customer {
+
+}
