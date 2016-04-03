ccd2a6803284145f79f719a23217d949d2c52608
Koen Hendriks <mailnaarkoen@gmail.com>
2016-04-03 23:15:15 +0200

    Updated documentation

--- PassHolder.java
+++ PassHolder.java
@@ -12,50 +12,92 @@ public class PassHolder extends Customer
     private String city;
     private boolean passHolder;
 
+    /**
+     * Constructor of the PassHolder
+     *
+     * @param name String with the customer
+     * @param address String with the customers address
+     * @param city String with the city of the customer
+     */
     public PassHolder(String name, String address, String city)
     {
-        // this customer receives his unique license plate
+        // The customer receives his unique license plate
         super();
         this.name = name;
         this.address = address;
         this.city = city;
 
-        // this customer is now a pass holder
+        // The customer is now a pass holder
         this.passHolder = true;
     }
 
-    // getters and setters
+    /**
+     * Get the customers name
+     *
+     * @return String with the name of the customer
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the customers name
+     *
+     * @param name String with the customers name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get the customers address
+     *
+     * @return String with the address of the customer
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Set the customers address
+     *
+     * @param address String with the address of the customer
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * Get the customers city
+     *
+     * @return String with the customers city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Set the customers city
+     *
+     * @param city String with the city of the customer
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Check if the customer is a pass holder,
+     *
+     * @return passHolder whether the customer is a pass holder
+     */
     public boolean isPassHolder() {
         return passHolder;
     }
 
     /**
      * Method for setting Pass Holder status.
-     * TODO Use this to implement expiration method
+     *
+     * @todo Use this to implement expiration method
      * @param passHolder Is this customer a Pass Holder?
      */
     public void setPassHolder(boolean passHolder) {
