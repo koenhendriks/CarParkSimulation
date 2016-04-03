987c4798f14d7f713f91cda050974f78b8f6582c
Koen Hendriks <mailnaarkoen@gmail.com>
2016-04-03 22:48:25 +0200

    Added documentation

--- Car.java
+++ Car.java
@@ -1,3 +1,8 @@
+/**
+ * This is the Car class, it represents a car that enters, pays and leaves the garage.
+ * It has a certain location in the parking garage and a certain amount of time that
+ * it stays in the garage.
+ */
 public abstract class Car {
 
     // Location for the Car object.
@@ -24,22 +29,47 @@ public abstract class Car {
         return location;
     }
 
+    /**
+     * Set the location of a car in the parking garage.
+     *
+     * @param location Location of the car
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Get the amount of minutes the car is still going to be in the garage.
+     *
+     * @return int amount of minutes left
+     */
     public int getMinutesLeft() {
         return minutesLeft;
     }
 
+    /**
+     * Set the amount of minutes the car is going to stay in the garage.
+     *
+     * @param minutesLeft int amount of minutes the car is going to stay.
+     */
     public void setMinutesLeft(int minutesLeft) {
         this.minutesLeft = minutesLeft;
     }
 
+    /**
+     * Check if the car is paying at the moment.
+     *
+     * @return boolean if the car is paying
+     */
     public boolean getIsPaying() {
         return isPaying;
     }
 
+    /**
+     * Tel the car that it is paying.
+     *
+     * @param isPaying boolean whether the car is paying or not
+     */
     public void setIsPaying(boolean isPaying) {
         this.isPaying = isPaying;
     }
