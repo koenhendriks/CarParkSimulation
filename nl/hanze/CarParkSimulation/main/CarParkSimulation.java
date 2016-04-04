3b590e7b27e1adf15328adf9ee52c9e2ec06edc1
Koen Hendriks <info@koenhendriks.com>
2016-04-04 21:53:38 +0200

    Added language class to the MVC and created the main CarParkSimulation constructor

--- nl/hanze/CarParkSimulation/main/CarParkSimulation.java
+++ nl/hanze/CarParkSimulation/main/CarParkSimulation.java
@@ -2,11 +2,15 @@ package nl.hanze.CarParkSimulation.main;
 
 import nl.hanze.CarParkSimulation.controller.AbstractController;
 import nl.hanze.CarParkSimulation.controller.Controller;
+import nl.hanze.CarParkSimulation.localization.en.Language;
 import nl.hanze.CarParkSimulation.logic.AbstractModel;
 import nl.hanze.CarParkSimulation.logic.CarPark;
 import nl.hanze.CarParkSimulation.view.AbstractView;
+import nl.hanze.CarParkSimulation.view.CarParkView;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Class CarParkSimulation
@@ -22,9 +26,50 @@ public class CarParkSimulation {
     private AbstractController carParkController;
 
     public CarParkSimulation() {
+
+        /**
+         * Create the model, view and controller that
+         * we need for the Car Park Simulation
+         */
         this.carParkModel = new CarPark();
         this.carParkController = new Controller(carParkModel);
-        this.
+        this.carParkView = new CarParkView(carParkModel);
+
+        /**
+         * Create the JFrame that will display the views
+         * and add these views to this JFrame
+         */
+        screen = new JFrame("Car Park Simulation");
+        screen.setSize(1200,750);
+        screen.setLayout(null);
+        screen.getContentPane().add(carParkView);
+
+        /**
+         * Add a window listener to the SimulatorView so we can send
+         * a confirmation to the user so we know they are sure if
+         * they want to close the Car Park Simulation
+         */
+        screen.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int confirm = JOptionPane.showOptionDialog(carParkView,
+                        Language.get("confirmExit"),
+                        Language.get("confirmExitTitle"), JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
+                if (confirm == 0) {
+                    System.exit(0); // 0 when execution went fine;
+                }
+            }
+        });
+
+        /**
+         * We only want to exit the application if the user has
+         * confirmed our message, otherwise we don't want to
+         * do anything, so we change the default close
+         */
+        screen.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+
+        screen.setVisible(true);
 
     }
 }
