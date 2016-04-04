7b372600f724dc3ab58b207e925b77468d6e272c
Koen Hendriks <info@koenhendriks.com>
2016-04-04 21:19:26 +0200

    Updated docs

--- nl/hanze/CarParkSimulation/controller/AbstractController.java
+++ nl/hanze/CarParkSimulation/controller/AbstractController.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 /**
  * Class AbstractController
+ * This is the abstract controller class that all controllers should extend.
  *
  * @author Koen Hendriks
  * @version 0.1 (04-04-2016)
