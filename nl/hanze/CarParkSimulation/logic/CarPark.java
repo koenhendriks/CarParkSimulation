f0318e01a83818ab4f3d741b184f3205bc4c64fc
Koen Hendriks <info@koenhendriks.com>
2016-04-04 21:35:35 +0200

    Created model for the car park

---
+++ nl/hanze/CarParkSimulation/logic/CarPark.java
@@ -0,0 +1,15 @@
+package nl.hanze.CarParkSimulation.logic;
+
+
+import nl.hanze.CarParkSimulation.view.AbstractView;
+
+import java.util.List;
+
+/**
+ * Class CarPark
+ *
+ * @author Koen Hendriks
+ * @version 0.1 (04-04-2016)
+ */
+public class CarPark extends AbstractModel{
+}
