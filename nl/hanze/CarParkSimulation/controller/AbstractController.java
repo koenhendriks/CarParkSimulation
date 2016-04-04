b369dc1fa31a0d3b2f9b59fbcfdc45cf6ce21539
Koen Hendriks <info@koenhendriks.com>
2016-04-04 21:13:34 +0200

    Controller should extend JPanel for GUI and views

--- nl/hanze/CarParkSimulation/controller/AbstractController.java
+++ nl/hanze/CarParkSimulation/controller/AbstractController.java
@@ -1,10 +1,12 @@
 package nl.hanze.CarParkSimulation.controller;
 
+import javax.swing.*;
+
 /**
  * Class AbstractController
  *
  * @author Koen Hendriks
  * @version 0.1 (04-04-2016)
  */
-public class AbstractController {
+public class AbstractController extends JPanel {
 }
