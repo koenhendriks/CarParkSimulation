da9acdede83bf73af3fc8ce685fc21909a8cc75a
Koen Hendriks <info@koenhendriks.com>
2016-04-04 21:11:35 +0200

    Fixed javadoc generation error on the date format

--- nl/hanze/CarParkSimulation/view/AbstractView.java
+++ nl/hanze/CarParkSimulation/view/AbstractView.java
@@ -4,7 +4,7 @@ package nl.hanze.CarParkSimulation.view;
  * Class AbstractView
  *
  * @author Koen Hendriks
- * @version 0.1 (${day}-${month}-${year})
+ * @version 0.1 (04-04-2016)
  */
 public class AbstractView {
 }
