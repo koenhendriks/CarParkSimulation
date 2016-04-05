146ee02bad4a484e16dc923a53c3b520cc954e13
Ruben Buisman <rbuisman95@gmail.com>
2016-04-05 12:09:19 +0200

    Added Car.java

---
+++ nl/hanze/CarParkSimulation/logic/Car.java
@@ -0,0 +1,20 @@
+package nl.hanze.CarParkSimulation.logic;
+
+/**
+ * Class Car
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (04-04-2016)
+ */
+
+/**
+ * This is the Car class, it represents a car that enters, pays and leaves the car park.
+ * It has a certain location in the parking car park and a certain amount of time that
+ * it stays in the car park.
+ */
+public abstract class Car extends AbstractModel {
+
+
+
+
+}
