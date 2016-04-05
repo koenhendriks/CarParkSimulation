32dc5649b6b8ed555fde87814fd8c347cc4e8005
Ruben Buisman <rbuisman95@gmail.com>
2016-04-05 12:28:53 +0200

    Added CarQueue to MVC structure

---
+++ nl/hanze/CarParkSimulation/logic/CarQueue.java
@@ -0,0 +1,18 @@
+package nl.hanze.CarParkSimulation.logic;
+
+/**
+ * Class CarQueue
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (05-04-2016)
+ */
+
+/**
+ * Class for creating different Queue collections of Car objects.
+ * The class is used for determining cars that need to enter the
+ * car park, need to pay their fee or want to exit the car park.
+ */
+
+public class CarQueue extends AbstractModel {
+
+}
