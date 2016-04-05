6af68a4ddaf4a7a01b385870cb3c4e9c49660aab
Ruben Buisman <rbuisman95@gmail.com>
2016-04-05 14:22:56 +0200

    Created new CarType view

---
+++ nl/hanze/CarParkSimulation/view/CarTypeView.java
@@ -0,0 +1,10 @@
+package nl.hanze.CarParkSimulation.view;
+
+/**
+ * Class CarTypeView
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (05-04-2016)
+ */
+public class CarTypeView extends AbstractView {
+}
