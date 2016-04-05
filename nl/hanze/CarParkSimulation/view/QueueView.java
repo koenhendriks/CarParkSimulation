0bdae1c09b930d03ccbefd20fadb94f86c9e4474
Ruben Buisman <rbuisman95@gmail.com>
2016-04-05 14:27:53 +0200

    Added QueueView to MVC

---
+++ nl/hanze/CarParkSimulation/view/QueueView.java
@@ -0,0 +1,12 @@
+package nl.hanze.CarParkSimulation.view;
+
+/**
+ * Class QueueView
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (05-04-2016)
+ */
+
+public class QueueView extends AbstractView {
+
+}
