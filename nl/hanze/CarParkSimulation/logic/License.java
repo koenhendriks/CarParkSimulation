31ca08e5b218c6389056a2f3489af1c19686ae34
Ruben Buisman <rbuisman95@gmail.com>
2016-04-05 12:19:51 +0200

    Created License.java, Location.java and PassHolder.java in the new MVC structure.

---
+++ nl/hanze/CarParkSimulation/logic/License.java
@@ -0,0 +1,15 @@
+package nl.hanze.CarParkSimulation.logic;
+
+/**
+ * Class License
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (05-04-2016)
+ */
+
+/**
+ * This class creates a license plate.
+ */
+
+public class License {
+}
---
+++ nl/hanze/CarParkSimulation/logic/PassHolder.java
@@ -0,0 +1,15 @@
+package nl.hanze.CarParkSimulation.logic;
+
+/**
+ * Class PassHolder
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (05-04-2016)
+ */
+
+/**
+ * This class
+ */
+
+public class PassHolder {
+}
