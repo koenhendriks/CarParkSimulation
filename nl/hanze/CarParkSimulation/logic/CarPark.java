7fbeb2d9897ce5509bb169e5679ee171be2781c3
Koen Hendriks <info@koenhendriks.com>
2016-04-05 13:21:38 +0200

    Basic model of CarPark

--- nl/hanze/CarParkSimulation/logic/CarPark.java
+++ nl/hanze/CarParkSimulation/logic/CarPark.java
@@ -13,4 +13,47 @@ import java.util.List;
  */
 public class CarPark extends AbstractModel{
 
+    private int numberOfFloors;
+    private int numberOfRows;
+    private int numberOfPlaces;
+
+    /**
+     * Constructor of the CarPark model expecting the floors, rows and places.
+     *
+     * @param numberOfFloors int with the amount of floors to draw
+     * @param numberOfRows   int with the amount of rows per floor to draw
+     * @param numberOfPlaces int with the amount of places per row to draw
+     */
+    public CarPark(int numberOfFloors, int numberOfRows, int numberOfPlaces) {
+        this.numberOfFloors = numberOfFloors;
+        this.numberOfRows = numberOfRows;
+        this.numberOfPlaces = numberOfPlaces;
+    }
+
+    /**
+     * Get number of floors
+     *
+     * @return int number of floors in the car park
+     */
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    /**
+     * Get number of rows
+     *
+     * @return int number of rows in the car park
+     */
+    public int getNumberOfRows() {
+        return numberOfRows;
+    }
+
+    /**
+     * Get number of places
+     *
+     * @return int number of places in the car park
+     */
+    public int getNumberOfPlaces() {
+        return numberOfPlaces;
+    }
 }
