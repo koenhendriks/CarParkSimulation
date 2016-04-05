169abe1b1e6d9644ec73d7662998f10fa0b4efd2
Ruben Buisman <rbuisman95@gmail.com>
2016-04-05 13:10:24 +0200

    Added methods to the MVC CarQueue class

--- nl/hanze/CarParkSimulation/logic/CarQueue.java
+++ nl/hanze/CarParkSimulation/logic/CarQueue.java
@@ -1,11 +1,16 @@
 package nl.hanze.CarParkSimulation.logic;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Class CarQueue
  *
  * @author Ruben Buisman
  * @version 0.1 (05-04-2016)
  */
+
+
 
 /**
  * Class for creating different Queue collections of Car objects.
@@ -14,5 +19,25 @@ package nl.hanze.CarParkSimulation.logic;
  */
 
 public class CarQueue extends AbstractModel {
+
+    // Queue collection Car objects.
+    private Queue<Car> queue = new LinkedList<>();
+
+    /**
+     * Method for checking whether a car has been added to the Queue.
+     * @param car added to the Queue
+     * @return true or false
+     */
+    public boolean addCar(Car car) {
+        return queue.add(car);
+    }
+
+    /**
+     * Method for obtaining a Car object from a Queue.
+     * @return Car object from the Queue
+     */
+    public Car removeCar() {
+        return queue.poll();
+    }
 
 }
