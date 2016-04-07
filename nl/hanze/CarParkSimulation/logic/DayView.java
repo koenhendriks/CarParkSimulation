b211180398835caee94754874df6a906a1d19312
Ruben Buisman <rbuisman95@gmail.com>
2016-04-07 15:05:41 +0200

    Created new labels, set the bounds and added to the view

--- nl/hanze/CarParkSimulation/logic/DayView.java
+++ nl/hanze/CarParkSimulation/logic/DayView.java
@@ -28,9 +28,47 @@ public class DayView extends AbstractView {
      */
     public DayView(CarPark model) {
         super(model);
+        this.size = new Dimension(100, 100);
 
+        /**
+         * Create labels
+         */
+        this.titleLabel = new JLabel();
+        this.time = new JLabel();
+        this.day = new JLabel();
+        this.week = new JLabel();
+        this.year = new JLabel();
 
+        /**
+         * Set font for title label
+         */
+        titleLabel.setFont(new Font("Serif", Font.PLAIN, 20));
 
+        /**
+         * Set the location for the labels
+         */
+        titleLabel.setBounds(10, 5, 200, 20);
+        time.setBounds(10,40,200,20);
+        day.setBounds(10,60,200,20);
+        week.setBounds(10,80,200,20);
+        year.setBounds(10,100,200,20);
+
+        /**
+         * Add the labels to the view
+         */
+        add(titleLabel);
+        add(time);
+        add(day);
+        add(week);
+        add(year);
+
+        /**
+         * Set the title
+         */
+        titleLabel.setText("Running time");
     }
 
+    public void updateView(){
+
+    }
 }
