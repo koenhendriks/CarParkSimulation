5dfd681d0d62c2f74e8e35ddb3376f3027fc041c
Joey Boum Bletterman <j.r.boum.bletterman@st.hanze.nl>
2016-04-11 16:35:15 +0100

    Updated interface for GridView class. (#39)

--- nl/hanze/CarParkSimulation/view/GridView.java
+++ nl/hanze/CarParkSimulation/view/GridView.java
@@ -8,17 +8,20 @@ import java.awt.geom.Line2D;
 /**
  * Class GridView
  *
- * @author Koen Hendriks
- * @version 0.1 (06-04-2016)
+ * This is a class for a grid display.
+ *
+ * @author Koen Hendriks, Joey Boum Bletterman
+ * @version 0.2 (11-04-2016)
  */
-public class GridView extends AbstractView{
-
+public class GridView extends AbstractView
+{
+    // grid dimensions
     private Dimension size;
 
     /**
-     * Constructor of GridView that expects a model belonging to this view
+     * Constructor of GridView that expects a model belonging to this view.
      *
-     * @param model AbstractModel that belongs to this view
+     * @param model AbstractModel that belongs to this view.
      */
     public GridView(AbstractModel model) {
         super(model);
@@ -29,36 +32,32 @@ public class GridView extends AbstractView{
      * Constructor of GridView that expects a model belonging to this view
      * and a given width and height to set the grid to.
      *
-     * @param model AbstractModel that belongs to this view
-     * @param width int width of the application
-     * @param height int height of the application
+     * @param model AbstractModel that belongs to this view.
+     * @param width int width of the application.
+     * @param height int height of the application.
      */
     public GridView(AbstractModel model, int width, int height) {
         super(model);
         this.size = new Dimension(width,height);
     }
 
-    @Override
     /**
      * Paint the grid on the JPanel
      */
+    @Override
     public void paint(Graphics g) {
         super.paint(g);
 
         Graphics2D g2 = (Graphics2D) g;
 
-        /**
-         * Draw the vertical lines of the grid
-         */
-        for(int i = 10; i < size.width; i=i+10){
+        // draw the vertical lines of the grid
+        for (int i = 10; i < size.width; i=i+10){
             Line2D line = new Line2D.Float(i,0,i,size.height);
             g2.draw(line);
         }
 
-        /**
-         * Draw the horizontal lines of the grid
-         */
-        for(int i = 10; i < size.height; i=i+10){
+        // draw the horizontal lines of the grid
+        for (int i = 10; i < size.height; i=i+10){
             Line2D line = new Line2D.Float(0,i,size.width,i);
             g2.draw(line);
         }
