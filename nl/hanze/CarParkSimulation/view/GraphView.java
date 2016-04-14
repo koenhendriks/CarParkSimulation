dfe1268028a0d6daa2adc65e2a9e7b029322fd16
Koen Hendriks <info@koenhendriks.com>
2016-04-14 22:45:18 +0200

    Added extra line for reservations

--- nl/hanze/CarParkSimulation/view/GraphView.java
+++ nl/hanze/CarParkSimulation/view/GraphView.java
@@ -7,10 +7,7 @@ import nl.hanze.CarParkSimulation.logic.Time;
 
 import java.awt.*;
 import java.awt.geom.Line2D;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 /**
  * Class GridView
@@ -29,12 +26,15 @@ public class GraphView extends AbstractView
     private Graphics2D graphic = null;
     private int maxCars = 0 ;
     private int lastTotalCars = 1;
-    private static int lastMinute = 0;
-    private static LinkedList<Line2D> lines;
+    private int lastTotalRevenues = 1;
+    private static int lastCarMinute = 0;
+    private static LinkedList<Line2D> totalCarLines;
+    private static LinkedList<Line2D> reservationLines;
     private Image image;
     private static Line2D yAxis;
     private static Line2D xAxis;
-    private static int drawnGraphs = 0;
+    private static int drawnCarGraphs = 0;
+    private static int drawnreservationLines = 0;
     private final int chartXOffset = 30;
     private final int chartYOffset = 40;
     private final int yZero = 170;
@@ -52,13 +52,14 @@ public class GraphView extends AbstractView
         this.size = new Dimension(1200,180);
         this.maxCars = CarPark.getNumberOfFloors() * CarPark.getNumberOfPlaces() * CarPark.getNumberOfRows();
         this.image = createImage(size.width,size.height);
-        lines = new LinkedList<>();
+        totalCarLines = new LinkedList<>();
+        reservationLines = new LinkedList<>();
 
         yAxis = new Line2D.Float(chartXOffset, chartYOffset, chartXOffset, yZero);
         xAxis = new Line2D.Float(chartXOffset, yZero, 1190, yZero);
 
-        lines.add(yAxis);
-        lines.add(xAxis);
+        totalCarLines.add(yAxis);
+        totalCarLines.add(xAxis);
     }
 
     /**
@@ -76,13 +77,14 @@ public class GraphView extends AbstractView
         this.size = new Dimension(width,height);
         this.maxCars = CarPark.getNumberOfFloors() * CarPark.getNumberOfPlaces() * CarPark.getNumberOfRows();
         this.image = createImage(size.width,size.height);
-        this.lines = new LinkedList<>();
+        totalCarLines = new LinkedList<>();
+        reservationLines = new LinkedList<>();
 
         yAxis = new Line2D.Float(chartXOffset, chartYOffset, chartXOffset, yZero);
         xAxis = new Line2D.Float(chartXOffset, yZero, 1190, yZero);
 
-        lines.add(yAxis);
-        lines.add(xAxis);
+        totalCarLines.add(yAxis);
+        totalCarLines.add(xAxis);
     }
 
     @Override
@@ -99,7 +101,7 @@ public class GraphView extends AbstractView
 
         int runningMinutes = Integer.valueOf(time.getRunningMinutes());
 
-        if(runningMinutes > lastMinute) {
+        if(runningMinutes > lastCarMinute) {
 
             // create a new car park image if the size has changed.
             image = createImage(size.width, size.height);
@@ -111,11 +113,16 @@ public class GraphView extends AbstractView
             g.drawString(Language.get("graphTotalCars"), 10,25);
 
             int totalCars = carPark.getTotalCars();
+            int reservations = carPark.getTotalReservationIndex();
 
             double carPercentOld = ((double) lastTotalCars / (double) maxCars) * 100;
             double carPercentNew = ((double) totalCars / (double) maxCars) * 100;
 
+            double reservationPercentOld = ((double) lastTotalRevenues / (double) maxCars) * 100;
+            double reservationPercentNew = ((double) reservations / (double) maxCars) * 100;
+
             int labelPercent = (int) Math.floor(carPercentNew);
+            int reservationPercent = (int) Math.floor(reservationPercentNew);
 
             for(int i = 0; i < 101; i = i +10){
                 g.drawString(""+i+"%", 0, (yZero - (int) Math.floor(1.3 * i) ));
@@ -124,36 +131,58 @@ public class GraphView extends AbstractView
             int oldCars = (int) Math.floor(1.3 * carPercentOld);
             int newCars = (int) Math.floor(1.3 * carPercentNew);
 
-            int xFrom = (lastMinute + chartXOffset) - (drawnGraphs * (size.width - chartXOffset));
+            int oldReservations = (int) Math.floor(1.3 * reservationPercentOld);
+            int newReservations = (int) Math.floor(1.3 * reservationPercentNew);
+
+            int xFrom = (lastCarMinute + chartXOffset) - (drawnCarGraphs * (size.width - chartXOffset));
             int yFrom = yZero - oldCars;
 
+            int xFromReservation = (lastCarMinute + chartXOffset) - (drawnreservationLines * (size.width - chartXOffset));
+            int yFromReservation = yZero - oldReservations;
 
-            int xTo = (runningMinutes + chartXOffset)  - (drawnGraphs * (size.width - chartXOffset));
+            int xTo = (runningMinutes + chartXOffset)  - (drawnCarGraphs * (size.width - chartXOffset));
             int yTo = yZero - newCars;
 
+            int xToReservations = (runningMinutes + chartXOffset)  - (drawnreservationLines * (size.width - chartXOffset));
+            int yToReservations = yZero - newReservations;
+
             if(xTo > size.width){
-                drawnGraphs ++;
-                lines = new LinkedList<>();
-                lines.add(yAxis);
-                lines.add(xAxis);
+                drawnCarGraphs++;
+                totalCarLines = new LinkedList<>();
+                reservationLines = new LinkedList<>();
+                totalCarLines.add(yAxis);
+                totalCarLines.add(xAxis);
+
+                drawnreservationLines++;
             }
 
             Line2D line = new Line2D.Float(xFrom, yFrom, xTo, yTo);
+            Line2D lineReservation = new Line2D.Float(xFromReservation, yFromReservation, xToReservations, yToReservations);
 
-            lines.add(line);
+            totalCarLines.add(line);
+            reservationLines.add(lineReservation);
 
             g.drawString(labelPercent+"% at "+time.getCurrentTime(),xTo-10,yTo-20);
+            g.drawString(reservationPercent+"% at "+time.getCurrentTime(),xToReservations-10,yToReservations-20);
 
-            if(!lines.isEmpty()){
-                for (Line2D line2D : lines) {
+            if(!totalCarLines.isEmpty()){
+                for (Line2D line2D : totalCarLines) {
                     g.draw(line2D);
                 }
             }
 
-            lastMinute = runningMinutes;
+            if(!reservationLines.isEmpty()){
+                for (Line2D line2D : reservationLines) {
+                    g.draw(line2D);
+                }
+            }
+
+            lastCarMinute = runningMinutes;
             lastTotalCars = totalCars;
+            lastTotalRevenues = reservations;
 
             lastTotalCars++;
+            lastTotalRevenues++;
 
             setVisible(true);
             super.updateView();
@@ -161,10 +190,12 @@ public class GraphView extends AbstractView
     }
 
     public static void reset(){
-        lastMinute = 0;
-        drawnGraphs = 0;
-        lines = new LinkedList<>();
-        lines.add(yAxis);
-        lines.add(xAxis);
+        lastCarMinute = 0;
+        drawnCarGraphs = 0;
+        drawnreservationLines = 0;
+        totalCarLines = new LinkedList<>();
+        reservationLines = new LinkedList<>();
+        totalCarLines.add(yAxis);
+        totalCarLines.add(xAxis);
     }
 }
