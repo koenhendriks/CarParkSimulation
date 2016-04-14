fd7b74e5343d30fb9506c19dc3796e8e67a2bcd9
Ruben Buisman <rbuisman95@gmail.com>
2016-04-14 13:46:39 +0200

    Created QueueLengthView

---
+++ nl/hanze/CarParkSimulation/view/QueueLengthView.java
@@ -0,0 +1,16 @@
+package nl.hanze.CarParkSimulation.view;
+
+import nl.hanze.CarParkSimulation.logic.AbstractModel;
+
+/**
+ * Class QueueLengthView
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (14-04-2016)
+ */
+public class QueueLengthView extends AbstractView {
+
+    public QueueLengthView(AbstractModel model) {
+        super(model);
+    }
+}
