7ff19edf4d08713887fa22a538468e01b39c8bd9
Joey Boum Bletterman <j.r.boum.bletterman@st.hanze.nl>
2016-04-14 13:20:05 +0200

    Updated Legend. (#47)

--- nl/hanze/CarParkSimulation/view/LegendView.java
+++ nl/hanze/CarParkSimulation/view/LegendView.java
@@ -18,8 +18,8 @@ public class LegendView extends AbstractView
     private JLabel title;
     private JLabel regular;
     private JLabel passholder;
-    private JLabel reserved;
-    private JLabel used;
+    private JLabel albert;
+    private JLabel opera;
 
     public LegendView(CarPark model) {
         super(model);
@@ -27,19 +27,19 @@ public class LegendView extends AbstractView
         title = new JLabel(Language.get("legTit"));
         regular = new JLabel(Language.get("legReg"));
         passholder = new JLabel(Language.get("legPas"));
-        reserved = new JLabel(Language.get("legRes"));
-        used = new JLabel(Language.get("legUse"));
+        albert = new JLabel(Language.get("legAh"));
+        opera = new JLabel(Language.get("legOp"));
 
         add(title);
         add(regular);
         add(passholder);
-        add(reserved);
-        add(used);
+        add(albert);
+        add(opera);
 
         title.setBounds(0,0,150,50);
         regular.setBounds(0,50,150,50);
         passholder.setBounds(0,70,150,50);
-        reserved.setBounds(0,90,150,50);
-        used.setBounds(0,110,150,50);
+        albert.setBounds(0,90,150,50);
+        opera.setBounds(0,110,150,50);
     }
 }
