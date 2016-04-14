6c64d43b4d3a12ac28fd742dc7232868256f50a1
Ruben Buisman <rbuisman95@gmail.com>
2016-04-14 13:47:47 +0200

    Added updateView to QueueLengthView

--- nl/hanze/CarParkSimulation/view/QueueLengthView.java
+++ nl/hanze/CarParkSimulation/view/QueueLengthView.java
@@ -10,7 +10,22 @@ import nl.hanze.CarParkSimulation.logic.AbstractModel;
  */
 public class QueueLengthView extends AbstractView {
 
+
+    /**
+     * Constructor of AbstractView that expects a model belonging to this view.
+     *
+     * @param model AbstractModel that belongs to this view.
+     */
     public QueueLengthView(AbstractModel model) {
         super(model);
     }
+
+
+    /**
+     * Gets called by the super model class when something needs to be updated.
+     */
+    public void updateView(){
+
+    }
+
 }
